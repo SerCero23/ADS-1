6bf85963b33a3c7c64c979f024b5828796029d0a
SerCero23 <62762144+SerCero23@users.noreply.github.com>
2020-03-28 20:47:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,7 @@ int cbinsearch(int* arr, int size, int value) {
     int n = 0;
     while (El_First <= El_Last) {
     int Mid = (El_First+El_Last)/2;
-    if(arr[Mid]==value)
+    if(arr[Mid]==value){
     n+=1;
      int k =1;
         while(arr[Mid+k]==value){
@@ -15,6 +15,7 @@ int cbinsearch(int* arr, int size, int value) {
         n+=1;
         k++;
         }
+        return n;}
     if(arr[Mid]<value)
     El_Last = Mid-1;
     else
